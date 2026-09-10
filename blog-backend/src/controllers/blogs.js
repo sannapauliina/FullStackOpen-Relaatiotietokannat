d24781809e99529f68b12ab58fbb6bed854c2674
Sanna Hiltunen <sannap.hiltunen@gmail.com>
2026-09-10 15:03:27 +0300

    Tehtävä 13.11: Lisätty blogin poiston rajoitus vain blogin omistajalle

--- blog-backend/src/controllers/blogs.js
+++ blog-backend/src/controllers/blogs.js
@@ -36,4 +36,21 @@ router.post("/", tokenExtractor, userExtractor, async (req, res) => {
   res.status(201).json(blog);
 });
 
+router.delete("/:id", tokenExtractor, userExtractor, async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id);
+
+  if (!blog) {
+    return res.status(404).json({ error: "blog not found" });
+  }
+
+  if (blog.userId !== req.user.id) {
+    return res
+      .status(403)
+      .json({ error: "only the creator can delete a blog" });
+  }
+
+  await blog.destroy();
+  res.status(204).end();
+});
+
 module.exports = router;
