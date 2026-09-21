2628c58864f49a42f64b661defcd2e7aebf83887
Sanna Hiltunen <sannap.hiltunen@gmail.com>
2026-09-21 14:23:41 +0300

    Tehtävä 13.18: Varmistettu tietokannan alustus migraatioilla ja aikaleimat

--- blog-backend/migrations/20240909-create-blogs.js
+++ blog-backend/migrations/20240909-create-blogs.js
@@ -42,7 +42,6 @@ module.exports = {
       },
     });
   },
-
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable("blogs");
   },
--- blog-backend/migrations/20260907113417-create-users.js
+++ blog-backend/migrations/20260907113417-create-users.js
@@ -32,7 +32,6 @@ module.exports = {
       },
     });
   },
-
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable("users");
   },
