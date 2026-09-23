d509a9408e95ae6fac653ce8eed6871a71010a2e
Sanna Hiltunen <sannap.hiltunen@gmail.com>
2026-09-23 13:47:16 +0300

    Tehtävä 13.22: Lisätty lukulistan tiedot käyttäjän readings-listaukseen

--- blog-backend/src/controllers/users.js
+++ blog-backend/src/controllers/users.js
@@ -20,7 +20,7 @@ router.get("/:id", async (req, res) => {
       as: "readings",
       attributes: ["id", "url", "title", "author", "likes", "year"],
       through: {
-        attributes: [],
+        attributes: ["id", "read"],
       },
     },
   });
