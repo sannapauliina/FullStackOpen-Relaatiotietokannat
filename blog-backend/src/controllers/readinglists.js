45f9009ff29bdb4f84db7f1b83dd5715ba4e02bf
Sanna Hiltunen <sannap.hiltunen@gmail.com>
2026-09-24 14:58:23 +0300

    Tehtävä 13.23: Lisätty lukulistan read-tilan päivittäminen

--- blog-backend/src/controllers/readinglists.js
+++ blog-backend/src/controllers/readinglists.js
@@ -1,5 +1,8 @@
 const router = require("express").Router();
 
+const tokenExtractor = require("../middleware/tokenExtractor");
+const userExtractor = require("../middleware/userExtractor");
+
 const ReadingList = require("../models/readingList");
 const User = require("../models/user");
 const Blog = require("../models/blog");
@@ -29,6 +32,28 @@ router.post("/", async (req, res) => {
   });
 
   res.status(201).json(reading);
+});
+
+router.put("/:id", tokenExtractor, userExtractor, async (req, res) => {
+  const reading = await ReadingList.findByPk(req.params.id);
+
+  if (!reading) {
+    return res.status(404).json({
+      error: "reading list entry not found",
+    });
+  }
+
+  if (reading.userId !== req.user.id) {
+    return res.status(403).json({
+      error: "not allowed",
+    });
+  }
+
+  reading.read = req.body.read;
+
+  await reading.save();
+
+  res.json(reading);
 });
 
 module.exports = router;
